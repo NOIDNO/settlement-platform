6b991e06764d0a4b2628e8245fadd3c27e73b085
NOIDNO <59988656+NOIDNO@users.noreply.github.com>
2026-06-15 14:15:39 +0900

    정산플랫폼 프로젝트 생성
    프로젝트 생성

---
+++ src/main/java/com/ohy/settlement_platform/SettlementPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.ohy.settlement_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SettlementPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SettlementPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ohy/settlement_platform/SettlementPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohy.settlement_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SettlementPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
